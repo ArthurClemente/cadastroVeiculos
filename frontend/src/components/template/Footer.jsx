3e9f2eeaee23619c53d49bf35be815c3ee0f3b34
Arthur <arthurclementte@hotmail.com>
2021-04-28 15:31:07 -0300

    Adicionado o texto do rodapé.

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,5 @@ import React from 'react'
 
 export default props => 
     <footer className="footer">
-        Rodapé
+        <span>Desenvolvido por <strong>Arthur Clemente :)</strong></span>
     </footer>
