552223b1be8195a3290a933d897eb1b06a887ef4
Arthur <arthurclementte@hotmail.com>
2021-04-27 21:01:23 -0300

    Criados os componentes que renderizam a página.

---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react'
+
+import Nav from '../components/template/Nav'
+import Main from '../components/template/Main'
+import Footer from '../components/template/Footer'
+
+export default props =>
+    <div className="app">
+        <Nav />
+        <Main />
+        <Footer />
+    </div>
