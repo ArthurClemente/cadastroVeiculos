b4ff478df991c1b8d27bbeb696cfab93df5b91bf
Arthur <arthurclementte@hotmail.com>
2021-04-28 16:18:44 -0300

    Criado o componente Home que referencia o componente Main.

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import Main from '../template/Main'
+import React from 'react'
+
+export default props =>
+    <Main icon="home" title="Início">
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema de cadastro de veículos em uma concessionária.</p>
+    </Main>
