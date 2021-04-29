b95556f03c654f757e8cfb75453999e0738ab7f9
Arthur <arthurclementte@hotmail.com>
2021-04-29 17:15:46 -0300

    Criadas as primeiras funções que serão utilizadas para salvar o conteúdo do formulário da página.

--- frontend/src/components/veiculos/veiculoCrud.jsx
+++ frontend/src/components/veiculos/veiculoCrud.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 import Main from '../template/Main'
 
 const headerProps = {
@@ -6,7 +7,38 @@ const headerProps = {
     title: 'Cadastro de veículos'
 }
 
-export default class veiculoCrud extends Component {
+const baseUrl = 'http://localhost:3001/veiculos'
+
+const initialState = {
+    veiculo: { Modelo: '', Marca: '', Valor: '', Obsevações: '' },
+    list: []
+}
+
+export default class VeiculoCrud extends Component {
+    
+    state = { ...initialState }
+    
+    clear() {
+        this.setState({ veiculo: initialState.veiculo })
+    }
+
+    save() {
+        const veiculo = this.state.veiculo
+        const metodo = veiculo.id ? 'put' : 'post'
+        const url = veiculo.id ? `${baseUrl}/${veiculo.id}` : baseUrl
+        axios[metodo](url, veiculo)
+            .then(resp => {
+                const list = this.getUpdatedList(resp.data)
+                this.setState({ veiculo: initialState.veiculo, list })
+            })
+    }
+
+    getUpdatedList(veiculo) {
+        const list = this.state.list.filter(v => v.id !== veiculo.id)
+        list.unshift(veiculo)
+        return list
+    }
+    
     render() {
         return (
             <Main {...headerProps}>
